391621e5168cdb6a161a6f1c743d928dcdb1fb41
jeremyfelt <jeremy.felt@gmail.com>
2014-12-22 14:58:16 -0800

    When a new site is setup, check if SSL should be generated
    This is moved over from the original logic in the WSUWP Platform
    repository so that we can extend it with more custom functionality
    around actually generating the certificates.

--- wsuwp-ssl.php
+++ wsuwp-ssl.php
@@ -54,7 +54,33 @@ class WSUWP_SSL {
 	/**
 	 * Setup the class.
 	 */
-	public function __construct() {}
+	public function __construct() {
+		add_action( 'wpmu_new_blog', array( $this, 'determine_new_site_ssl' ), 10, 3 );
+	}
+
+	/**
+	 * Determine if a new site should be flagged for SSL configuration.
+	 *
+	 * If this domain has already been added for another site, we'll assume the SSL status
+	 * of that configuration and allow it to play out. If this is the first time for this
+	 * domain, then we should flag it as SSL disabled.
+	 *
+	 * @param $blog_id
+	 * @param $user_id
+	 * @param $domain
+	 */
+	public function determine_new_site_ssl( $blog_id, $user_id, $domain ) {
+		/* @type WPDB $wpdb */
+		global $wpdb;
+
+		$domain_exists = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE domain = %s AND blog_id != %d LIMIT 1", $domain, $blog_id ) );
+
+		if ( ! $domain_exists ) {
+			switch_to_blog( 1 );
+			update_option( $domain . '_ssl_disabled', 1 );
+			restore_current_blog();
+		}
+	}
 
 	/**
 	 * Given a server name, generate a private key and a matching CSR so that a
