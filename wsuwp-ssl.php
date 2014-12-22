6aaf11f271c79823b7f8ecb12caf66f4b98bafeb
jeremyfelt <jeremy.felt@gmail.com>
2014-12-22 13:47:40 -0800

    Create an intial class for the file

---
+++ wsuwp-ssl.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: WSUWP SSL
+Version: 0.0.0
+Plugin URI: http://web.wsu.edu
+Description: Manage SSL within the WSUWP Platform
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: http://web.wsu.edu
+*/
+
+class WSUWP_SSL {
+	public function __construct() {
+		
+	}
+}
+new WSUWP_SSL();
