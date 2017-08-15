55eafdd46e613682fe9547849194658a9712730f
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-15 13:35:48 -0700

    Update Grunt configuration, add PHPCS, WPCS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,80 @@
-module.exports = function(grunt) {
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			build: {
-				src: 'js/wsu-tls-site.js',
-				dest: 'js/wsu-tls-site.min.js'
+module.exports = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		phpcs: {
+			plugin: {
+				src: [ "./*.php", "./includes/*.php", "./tests/*.php" ]
+			},
+			options: {
+				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+				standard: "phpcs.ruleset.xml"
 			}
 		},
+
+		jscs: {
+			scripts: {
+				src: [ "Gruntfile.js", "js/wsu-tls-site.js" ],
+				options: {
+					preset: "jquery",
+					requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
+					maximumLineLength: 250
+				}
+			}
+		},
+
+		uglify: {
+			build: {
+				src: "js/wsu-tls-site.js",
+				dest: "js/wsu-tls-site.min.js"
+			}
+		},
+
 		jshint: {
-			files: ['js/wsu-tls-site.js'],
-			options: {
-				boss: true,
-				curly: true,
-				eqeqeq: true,
-				eqnull: true,
-				expr: true,
-				immed: true,
-				noarg: true,
-				onevar: false,
-				smarttabs: true,
-				trailing: true,
-				undef: true,
-				unused: true,
-				globals: {
-					jQuery: true,
-					console: true,
-					module: true,
-					document: true,
-					window:true
+			grunt_script: {
+				src: [ "Gruntfile.js" ],
+				options: {
+					curly: true,
+					eqeqeq: true,
+					noarg: true,
+					quotmark: "double",
+					undef: true,
+					unused: false,
+					node: true     // Define globals available when running in Node.
+				}
+			},
+			tls_script: {
+				src: [ "js/wsu-tls-site.js" ],
+				options: {
+					boss: true,
+					curly: true,
+					eqeqeq: true,
+					eqnull: true,
+					expr: true,
+					immed: true,
+					noarg: true,
+					onevar: false,
+					smarttabs: true,
+					trailing: true,
+					undef: true,
+					unused: true,
+					globals: {
+						jQuery: true,
+						console: true,
+						module: true,
+						document: true,
+						window:true
+					}
 				}
 			}
 		}
-	});
+	} );
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
 
 	// Default task(s).
-	grunt.registerTask('default', ['jshint', 'uglify']);
+	grunt.registerTask( "default", [ "phpcs", "jscs", "jshint", "uglify" ] );
 };
