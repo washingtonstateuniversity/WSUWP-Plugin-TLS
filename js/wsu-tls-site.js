89984a52e421ecc402b4f7d71ad1794be4f79d35
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-15 13:13:49 -0700

    Autofix JS formatting via JSCS ruleset

--- js/wsu-tls-site.js
+++ js/wsu-tls-site.js
@@ -1,76 +1,76 @@
 /* global Backbone, jQuery, _ */
 var wsuTLS = wsuTLS || {};
 
-(function (window, Backbone, $, _, wsuTLS) {
-	'use strict';
+( function( window, Backbone, $, _, wsuTLS ) {
+	"use strict";
 
-	wsuTLS.appView = Backbone.View.extend({
+	wsuTLS.appView = Backbone.View.extend( {
 
-		el: '.wsu-manage-tls',
+		el: ".wsu-manage-tls",
 
-		domain: '',
+		domain: "",
 
-		row: '',
+		row: "",
 
 		// Setup the events used in the overall application view.
 		events: {
-			'click #submit-add-domain': 'handle_submit_click',
-			'click .confirm_tls': 'handle_confirm_click',
-			'click .check_tls': 'handle_tls_check',
-			'click .view_csr': 'view_csr',
-			'click .view-csr-close': 'remove_csr_response',
-			'click .tls-status-close': 'remove_tls_response'
+			"click #submit-add-domain": "handle_submit_click",
+			"click .confirm_tls": "handle_confirm_click",
+			"click .check_tls": "handle_tls_check",
+			"click .view_csr": "view_csr",
+			"click .view-csr-close": "remove_csr_response",
+			"click .tls-status-close": "remove_tls_response"
 		},
 
 		handle_submit_click: function() {
-			this.domain = $('#add-domain').val();
+			this.domain = $( "#add-domain" ).val();
 			this.unconfirm_tls_domain( this.domain );
 		},
 
-		handle_confirm_click: function(evt) {
-			this.domain = $(evt.target).attr('data-domain');
-			this.row = $(evt.target).parents('tr').attr('id');
+		handle_confirm_click: function( evt ) {
+			this.domain = $( evt.target ).attr( "data-domain" );
+			this.row = $( evt.target ).parents( "tr" ).attr( "id" );
 
 			if ( true === window.confirm( "Removing " + this.domain + " from the TLS confirmation list." ) ) {
 				this.confirm_tls_domain( this.domain );
 			}
 		},
 
-		handle_tls_check: function(evt) {
-			this.domain = $(evt.target).attr('data-domain');
+		handle_tls_check: function( evt ) {
+			this.domain = $( evt.target ).attr( "data-domain" );
 
-			var ajax_nonce = $('#tls_ajax_nonce').val();
+			var ajax_nonce = $( "#tls_ajax_nonce" ).val();
 
 			var data = {
-				'action' : 'check_tls',
-				'domain' : this.domain,
-				'ajax_nonce' : ajax_nonce
+				"action": "check_tls",
+				"domain": this.domain,
+				"ajax_nonce": ajax_nonce
 			};
-			$.post(window.ajaxurl,data,this.check_tls_response);
+			$.post( window.ajaxurl, data, this.check_tls_response );
 		},
 
 		check_tls_response: function( response ) {
-			response = $.parseJSON(response);
+			response = $.parseJSON( response );
 			if ( response.success ) {
-				$('.tls-status-container-body').html( response.success );
-				$('.tls-status-container-wrapper').show();
+				$( ".tls-status-container-body" ).html( response.success );
+				$( ".tls-status-container-wrapper" ).show();
 			}
 		},
 
 		/**
 		 * Fire an ajax call to WordPress to find the generated CSR for display.
 		 */
-		view_csr: function(evt) {
-			this.domain = $(evt.target).attr('data-domain');
+		view_csr: function( evt ) {
+			this.domain = $( evt.target ).attr( "data-domain" );
 
-			var ajax_nonce = $('#tls_ajax_nonce').val();
+			var ajax_nonce = $( "#tls_ajax_nonce" ).val();
 
 			var data = {
-				'action' : 'view_csr',
-				'domain' : this.domain,
-				'ajax_nonce' : ajax_nonce
+				"action": "view_csr",
+				"domain": this.domain,
+				"ajax_nonce": ajax_nonce
 			};
-			$.post(window.ajaxurl,data,this.view_csr_response);
+			$.post( window.ajaxurl, data, this.view_csr_response );
 		},
 
 		/**
@@ -81,8 +81,8 @@ var wsuTLS = wsuTLS || {};
 		view_csr_response: function( response ) {
 			response = $.parseJSON( response );
 			if ( response.success ) {
-				$('.view-csr-container-body').html('<textarea>' + response.success + '</textarea>');
-				$('.view-csr-container-wrapper').show();
+				$( ".view-csr-container-body" ).html( "<textarea>" + response.success + "</textarea>" );
+				$( ".view-csr-container-wrapper" ).show();
 			}
 		},
 
@@ -90,16 +90,16 @@ var wsuTLS = wsuTLS || {};
 		 * Hide the container used to view the CSR.
 		 */
 		remove_csr_response: function() {
-			$('.view-csr-container-body').html('');
-			$('.view-csr-container-wrapper').hide();
+			$( ".view-csr-container-body" ).html( "" );
+			$( ".view-csr-container-wrapper" ).hide();
 		},
 
 		/**
 		 * Hide a container used to view the TLS status of a domain.
 		 */
 		remove_tls_response: function() {
-			$('.tls-status-container-body').html('');
-			$('.tls-status-container-wrapper').hide();
+			$( ".tls-status-container-body" ).html( "" );
+			$( ".tls-status-container-wrapper" ).hide();
 		},
 
 		/**
@@ -108,13 +108,13 @@ var wsuTLS = wsuTLS || {};
 		 * @param domain
 		 */
 		unconfirm_tls_domain: function( domain ) {
-			var ajax_nonce = $('#tls_ajax_nonce' ).val();
+			var ajax_nonce = $( "#tls_ajax_nonce" ).val();
 			var data = {
-				'action' : 'unconfirm_tls',
-				'domain' : domain,
-				'ajax_nonce' : ajax_nonce
+				"action": "unconfirm_tls",
+				"domain": domain,
+				"ajax_nonce": ajax_nonce
 			};
-			$.post(window.ajaxurl,data,this.handle_unconfirm_response);
+			$.post( window.ajaxurl, data, this.handle_unconfirm_response );
 		},
 
 		/**
@@ -125,7 +125,7 @@ var wsuTLS = wsuTLS || {};
 		handle_unconfirm_response: function( response ) {
 			response = $.parseJSON( response );
 			if ( response.success ) {
-				$('#add-domain' ).val('');
+				$( "#add-domain" ).val( "" );
 				window.location.reload();
 			} else if ( response.error ) {
 				$( "#tls-error-message" ).html( "<strong>Error:</strong> " + response.error );
@@ -138,15 +138,15 @@ var wsuTLS = wsuTLS || {};
 		 *
 		 * @param domain
 		 */
-		confirm_tls_domain: function ( domain ) {
-			var ajax_nonce = $('#tls_ajax_nonce' ).val();
+		confirm_tls_domain: function( domain ) {
+			var ajax_nonce = $( "#tls_ajax_nonce" ).val();
 			var data = {
-				'action' : 'confirm_tls',
-				'domain' : domain,
-				'ajax_nonce' : ajax_nonce
+				"action": "confirm_tls",
+				"domain": domain,
+				"ajax_nonce": ajax_nonce
 			};
 
-			$.post(window.ajaxurl,data,this.handle_confirm_response);
+			$.post( window.ajaxurl, data, this.handle_confirm_response );
 		},
 
 		/**
@@ -157,11 +157,11 @@ var wsuTLS = wsuTLS || {};
 		handle_confirm_response: function( response ) {
 			response = $.parseJSON( response );
 			if ( response.success ) {
-				$('#' + wsuTLS.app.row ).remove();
+				$( "#" + wsuTLS.app.row ).remove();
 			}
 		}
 
-	});
+	} );
 
 	wsuTLS.app = new wsuTLS.appView();
-})(window, Backbone, jQuery, _, wsuTLS);
+} )( window, Backbone, jQuery, _, wsuTLS );
